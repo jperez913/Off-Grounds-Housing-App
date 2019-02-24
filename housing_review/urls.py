0ec6be4c68214cd52d097d41d22595392f28b5b7
Will Define <wjd6ca@virginia.edu>
2019-02-24 16:40:56 -0500

    initial form generated

--- housing_review/urls.py
+++ housing_review/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create/', views.ReviewCreate.as_view(), name='create'),
+    path('createpost/', views.createpost, name='createpost'),
 ]
