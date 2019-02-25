50d901fc5b35667acff1f9e1b7590d879fcca9c8
Dale Wilson <dsw6ru@virginia.edu>
2019-02-25 18:13:08 -0500

    added url for all-reviews view

--- housing_review/urls.py
+++ housing_review/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('review/', views.ReviewView.as_view(), name='review'),
+    path('all-reviews/', views.allReviews.as_view(), name='all-review'),
 ]
