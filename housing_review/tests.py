523b02f1f04dc26cd3860406bfc7adae8221bc10
Will N <Kippenger@gmail.com>
2019-03-02 23:48:47 -0500

    basicTestsInclusion
    So I forgot to switch to my branch to make sure everything works but I will quickly commit these completd tests to master. I will finish up more later. Best of luck

--- housing_review/tests.py
+++ housing_review/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
-# Create your tests here.
+import datetime
+import unittest
+
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import User, Review
+
+class LoginTest(TestCase):
+    def testuserPass(self):
+        coocoo = User(first_name='John', last_name='Smith', email='test@test.edu')
+        self.assertEqual(coocoo.first_name, 'John')
+    def testuserFail(self):
+        coco = User(first_name='John', last_name='Smith', email='test@test.edu')
+        self.assertNotEqual(coco.first_name, 'Jo')
+
+class ReviewTest(TestCase):
+    def testgreatlyrated(self):
+        goodHomePass = Review(stars = 5)
+        self.assertGreaterEqual(goodHomePass.stars, 4)
+    def testLowRated(self):
+        badHome = Review(stars = 2)
+        self.assertLessEqual(badHome.stars, 2)
