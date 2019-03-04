9a88025f49d4ed0245e26f5fa1d9b41b5a5f08c9
Dale Wilson <dsw6ru@virginia.edu>
2019-03-04 10:46:54 -0500

    Added bootstrap to all reviews view

--- housing_review/urls.py
+++ housing_review/urls.py
@@ -10,11 +10,11 @@ from . import views
 #    url(r'^admin/', admin.site.urls),
 #    url(r'^login/$', LoginView.as_view(), name='login'),
 #    url(r'^logout/$', LogoutView.as_view(), name='logout'),
-#    url(r'^auth/', include('social_django.urls', namespace='social')), 
+#    url(r'^auth/', include('social_django.urls', namespace='social')),
 #    url(r'^$', home, name='home'),
 #]
 urlpatterns = [
     path('', views.index, name='index'),
     path('review/', views.ReviewView.as_view(), name='review'),
-    path('all_reviews/', views.allReviews.as_view(), name='all-review'),
+    path('all_reviews/', views.allReviews.as_view(), name='all-reviews'),
 ]
