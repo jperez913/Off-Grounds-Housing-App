dc59d6a9c1bf095c4787509ec56c55b9eb85de37
Will N <Kippenger@gmail.com>
2019-03-12 15:27:48 -0400

    ?

--- TheSheriff/settings.py
+++ TheSheriff/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'social_django',
+    #'social_django',
 ]
 
 MIDDLEWARE = [
