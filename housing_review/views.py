5a2f75c251462e68c7e3043f215cd902320a7a05
Will Define <wjd6ca@virginia.edu>
2019-03-30 18:40:11 -0400

    added the U and D in crud. also hid site behind login page

--- housing_review/views.py
+++ housing_review/views.py
@@ -2,26 +2,27 @@ from django.shortcuts import render
 from django.views import generic
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.utils import timezone
 from django.views.generic.list import ListView
 
 from .models import Review, NEIGHBORHOODS, UTILITIES, AMENITIES
 
-
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
-from django.http import HttpResponseRedirect
 from django.db import transaction
 from .models import Profile
-from .forms import UserForm,ProfileForm
+from .forms import UserForm, ProfileForm
+from django.utils.decorators import method_decorator
+
 
 @login_required
 def Home(request):
     return render(request, 'home/home.html')
+
 
 @login_required
 @transaction.atomic
@@ -43,64 +44,158 @@ def update_profile(request):
         'profile_form': profile_form
     })
 
+
+@login_required
 def Logout(request):
     logout(request)
     return HttpResponseRedirect('/')
 
 
-
-
+''' DEPRECATED
 def index(request):
   return HttpResponse("Hello, world. You're ready to review housing options now.")
 
 def map(request):
   return render(request, "housing_review/map.html", {'lat':38.0314867, 'lng':-78.5090342}) #38.0314867,-78.5090342
+'''
 
+
+@method_decorator(login_required, name='dispatch')
 class ReviewView(generic.View):
-  def get(self, request, *args, **kwargs):
-    return render(request, "housing_review/review_form.html", {'neighborhoods':NEIGHBORHOODS, 'utilities':UTILITIES, 'amenities':AMENITIES})
+    def get(self, request, *args, **kwargs):
+        return render(
+            request, "housing_review/review.html", {
+                'neighborhoods': NEIGHBORHOODS,
+                'utilities': UTILITIES,
+                'amenities': AMENITIES
+            })
 
-  def post(self, request, *args, **kwargs):
-    text = request.POST['text']
-    pub_date = timezone.now()
-    address = request.POST['address']
-    stars = int(request.POST['stars'])
-    price = float(request.POST['price'])
-    utilities_cost = float(request.POST['utilities_cost'])
-    bathrooms = int(request.POST['bathrooms'])
-    bedrooms = int(request.POST['bedrooms'])
+    def post(self, request, *args, **kwargs):
+        text = request.POST['text']
+        pub_date = timezone.now()
+        address = request.POST['address']
+        stars = int(request.POST['stars'])
+        price = float(request.POST['price'])
+        utilities_cost = float(request.POST['utilities_cost'])
+        bathrooms = int(request.POST['bathrooms'])
+        bedrooms = int(request.POST['bedrooms'])
 
-    hood_arr = []
-    util_arr = []
-    amen_arr = []
-    for hood in NEIGHBORHOODS:
-      x = request.POST.get(hood, "")
-      if x != "":
-        hood_arr.append(x)
-    for util in UTILITIES:
-      x = request.POST.get(util, "")
-      if x != "":
-        util_arr.append(x)
-    for amen in AMENITIES:
-      x = request.POST.get(amen, "")
-      if x != "":
-        amen_arr.append(x)
+        hood_arr = []
+        util_arr = []
+        amen_arr = []
+        for hood in NEIGHBORHOODS:
+            x = request.POST.get(hood, "")
+            if x != "":
+                hood_arr.append(x)
+        for util in UTILITIES:
+            x = request.POST.get(util, "")
+            if x != "":
+                util_arr.append(x)
+        for amen in AMENITIES:
+            x = request.POST.get(amen, "")
+            if x != "":
+                amen_arr.append(x)
+        user = User.objects.get(email=request.user.email)
+        distance_to_newcomb = 1.00  #Get This somehow
 
-    reviewer = 1 #get primary key
-    distance_to_newcomb = 1.00 #Get This somehow
+        neighborhood = ",".join(hood_arr)
+        utilities = ",".join(util_arr)
+        amenities = ",".join(amen_arr)
 
-    neighborhood = ",".join(hood_arr)
-    utilities = ",".join(util_arr)
-    amenities = ",".join(amen_arr)
+        r = Review(
+            text=text,
+            pub_date=pub_date,
+            neighborhood=neighborhood,
+            address=address,
+            stars=stars,
+            price=price,
+            utilities=utilities,
+            amenities=amenities,
+            utilities_cost=utilities_cost,
+            bathrooms=bathrooms,
+            bedrooms=bedrooms,
+            reviewer=user.pk,
+            distance_to_newcomb=distance_to_newcomb)
+        r.save()
 
-    #note no reviewer in this. Need to add this in later
-    r = Review(text=text, pub_date=pub_date, neighborhood=neighborhood, address=address, stars=stars, price=price, utilities=utilities, amenities=amenities, utilities_cost=utilities_cost, bathrooms=bathrooms, bedrooms=bedrooms, distance_to_newcomb=distance_to_newcomb)
-    r.save()
+        return HttpResponseRedirect(reverse('all-review'))
 
-    return HttpResponseRedirect(reverse('all-review'))
 
+@method_decorator(login_required, name='dispatch')
+class Manage(generic.View):
+    def get(self, request, *args, **kwargs):
+        user = User.objects.get(email=request.user.email)
+        Reviews = Review.objects.filter(reviewer=user.pk)
+        return render(
+            request, "housing_review/manage.html", {
+                'Reviews': Reviews,
+                'neighborhoods': NEIGHBORHOODS,
+                'utilities': UTILITIES,
+                'amenities': AMENITIES
+            })
+
+    def post(self, request, *args, **kwargs):
+        submit_type = request.POST['submit_type']
+        if submit_type == "update":
+            text = request.POST['text']
+            pub_date = timezone.now()
+            address = request.POST['address']
+            stars = int(request.POST['stars'])
+            price = float(request.POST['price'])
+            utilities_cost = float(request.POST['utilities_cost'])
+            bathrooms = int(request.POST['bathrooms'])
+            bedrooms = int(request.POST['bedrooms'])
+            hood_arr = []
+            util_arr = []
+            amen_arr = []
+            for hood in NEIGHBORHOODS:
+                x = request.POST.get(hood, "")
+                if x != "":
+                    hood_arr.append(x)
+            for util in UTILITIES:
+                x = request.POST.get(util, "")
+                if x != "":
+                    util_arr.append(x)
+            for amen in AMENITIES:
+                x = request.POST.get(amen, "")
+                if x != "":
+                    amen_arr.append(x)
+            user = User.objects.get(email=request.user.email)
+            distance_to_newcomb = 1.00  #Get This somehow
+            neighborhood = ",".join(hood_arr)
+            utilities = ",".join(util_arr)
+            amenities = ",".join(amen_arr)
+
+            pk = request.POST['pk']
+            review = Review.objects.get(pk=pk)
+            review.text = text
+            review.pub_date = pub_date
+            review.neighborhood = neighborhood
+            review.address = address
+            review.stars = stars
+            review.price = price
+            review.utilities = utilities
+            review.utilities_cost = utilities_cost
+            review.amenities = amenities
+            review.utilities = utilities
+            review.bathrooms = bathrooms
+            review.bedrooms = bedrooms
+            review.reviewer = user.pk
+            review.distance_to_newcomb = distance_to_newcomb
+            review.save()
+
+        elif submit_type == "delete":
+            pk = request.POST['pk']
+            review = Review.objects.get(pk=pk)
+            review.delete()
+
+        return HttpResponseRedirect(reverse('all-review'))
+
+
+@method_decorator(login_required, name='dispatch')
 class allReviews(ListView):
-  model = Review
-  template_name = 'housing_review/all_reviews.html'
-  def get_queryset(self):
-    return Review.objects.all()
+    model = Review
+    template_name = 'housing_review/all_reviews.html'
+
+    def get_queryset(self):
+        return Review.objects.all()
