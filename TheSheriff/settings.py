d73f8a5a751d4877b81c8f22afd1decac70707e8
Will N <Kippenger@gmail.com>
2019-04-08 11:39:47 -0400

    WE ARE IN MY GUY

--- TheSheriff/settings.py
+++ TheSheriff/settings.py
@@ -146,6 +146,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 try:
     django_heroku.settings(locals())
 except: ImportError
